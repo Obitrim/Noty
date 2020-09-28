b37957d8168589c1b971edd1120861c0bede3971
obitrim <pevangelist516@gmail.com>
2020-09-28 01:41:31 +0000

    added code view functionality

--- src/components/note/NotePreviewBoard.vue
+++ src/components/note/NotePreviewBoard.vue
@@ -1,3 +1,16 @@
 <template>
-	<div class="NotePreviewBoard bg-white">Read View</div>
+	<div class="NotePreviewBoard bg-white">
+		<button type="button" class="d-md-none" @click="$emit('goToNotes')">back</button>
+		<div class="p-3" v-html="previewedNote.content"></div>
+	</div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+	computed: {
+		...mapState(['previewedNote'])
+	}
+}
+</script>
