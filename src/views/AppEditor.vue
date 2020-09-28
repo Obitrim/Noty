d87d9064019ff33b6ee5a4fe0a00c693a24f193e
obitrim <pevangelist516@gmail.com>
2020-09-28 01:45:11 +0000

    don't know what i changed

--- src/views/AppEditor.vue
+++ src/views/AppEditor.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="AppEditor">
-    <vue-editor class="mx-2" v-model="noteContent"/>
-    <button type="button" class="saveBtn btn mx-2 my-4 ">Save</button>
+  <div class="AppEditor mt-2 d-flex">
+    <vue-editor class="mx-2" v-model="newNote.content"/>
+    <button type="button" class="saveBtn btn btn-lg mx-2 my-4 align-self-end" @click="saveNote">Save</button>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import { VueEditor } from 'vue2-editor';
 
 export default {
   components: { VueEditor },
 
-  data(){
-    return {
-      noteContent: ""
+  computed: {
+    ...mapState(['newNote'])
+  },
+
+  methods: {
+    saveNote(){
+      if(this.newNote.editing){
+        // edit note
+      } else {
+        // create new note
+      }
     }
   }
+
+
 }
 </script>
