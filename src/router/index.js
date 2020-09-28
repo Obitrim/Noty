9bf7e96a7bf5556d34b78fc8dc8701e7806e3d2e
obitrim <pevangelist516@gmail.com>
2020-09-28 01:41:59 +0000

    added navigation guard to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import store from "@/store";
 import Router from "vue-router";
 import routes from "./routes.js";
 
@@ -8,6 +9,14 @@ const router = new Router({
 	mode: "history",
 	// base: process.env.,
 	routes
+});
+
+router.beforeEach((to, from, next) => {
+	let categoryParam = to.params.category;
+	if(categoryParam){
+		store.commit('setFocusedCategory', categoryParam);
+	}
+	next();
 });
 
 export default router;
