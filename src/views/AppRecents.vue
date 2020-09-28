4efba5a70fca4db48925d2e4a8c72d403511945d
obitrim <pevangelist516@gmail.com>
2020-09-28 01:45:52 +0000

    refactored code to show default recent notes if none exists

--- src/views/AppRecents.vue
+++ src/views/AppRecents.vue
@@ -1,9 +1,16 @@
 <template>
 	<div class="AppRecents py-3 px-4">
 		<div class="row">
+			<div class="col-sm-6 col-md-4 col-xl-3 my-2" v-if="emptyRecentNotes">
+				<NoteCard :note="{
+					title: 'No notes were viewed recently',
+					category: 'Not Found'
+				}" :readBtn="false"/>
+			</div>
 			<div class="col-sm-6 col-md-4 col-xl-3 my-2" 
 				v-for="(note, index) in notes" 
 				:key="note.title + index"
+				v-else
 				>
 				<NoteCard :note="note"/>
 			</div>
@@ -19,7 +26,11 @@ export default {
 	components: { NoteCard },
 
 	computed: {
-		...mapState(["notes"])
+		...mapState(["recents"]),
+
+		emptyRecentNotes(){
+			return this.recents.length === 0;
+		}
 	}
 }
 </script>
