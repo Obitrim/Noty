7fb12a17f6133d07b3e424eefa3920dd83770414
obitrim <pevangelist516@gmail.com>
2020-09-28 01:43:02 +0000

    added data to status

--- src/store/state.js
+++ src/store/state.js
@@ -1,77 +1,66 @@
 export default {
 	navOpen: false,
-	notes: [
-		{
+
+	categorizedNotes: {
+		business: [{
+				title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Laudantium minima iusto, consequuntur expedita quaerat illo velit totam',
+				category: 'business',
+				content: `<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora</h1>
+					A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
+					<strong>Bold content</strong>
+					fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
+					minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
+				color: 'green'
+			},{
+				title: 'Lorem ipsum, dolor sit, amet consecteturma iusto, consequuntur expedita quaerat illo velit totam',
+				category: 'business',
+				content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
+					A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
+					fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
+					minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
+				color: 'orange'
+			},{
+				title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Laudantium minima iusto, consequuntur expedita quaerat illo velit totam',
+				category: 'business',
+				content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
+					A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
+					fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
+					minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
+				color: 'violet'	
+		}],
+		work: [{
 			title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Laudantium minima iusto, consequuntur expedita quaerat illo velit totam',
-			category: 'business',
+			category: 'work',
 			content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
 				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
 				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
 				minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
 			color: 'red'
-		},
-		{
-			title: 'Lorem ipsum, dolor sit, amet Paul adipisicing elit  consequuntur expedita quaerat illo velit totam',
+		},{
+			title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Laudantium minima iusto, consequuntur expedita quaerat illo velit totam',
+			category: 'work',
+			content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
+				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
+				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
+				minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
+			color: 'blue'
+		}],
+		school: [{
+			title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Laudantium minima iusto, consequuntur expedita quaerat illo velit totam',
 			category: 'school',
 			content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
 				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
 				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
 				minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
-			color: 'violet'
-		},
-		{
-			title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elitexpedita quaerat illo velit totam',
-			category: 'work',
-			content: `Lorem ipsum dolor, sit amet Paul adipisicing elit. Ad, minima dolor non animi culpa tempora. 
-				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
-				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
-				minima eaque tempore, quas laudantium suscipit angela, similique animi beatae`,
-			color: 'yellow'
-		},
-		{
-			title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Laudantium minima iusto, consequuntur expedita quaerat illo velit totam',
-			category: 'work',
-			content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
-				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
-				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
-				minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
 			color: 'red'
-		},
-		{
-			title: 'Lorem ipsum, dolor sit, amet consecteturma iusto, consequuntur expedita quaerat illo velit totam',
-			category: 'business',
-			content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
-				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
-				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
-				minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
-			color: 'orange'
-		},
-		{
-			title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elitquaerat illo velit totam',
-			category: 'school',
-			content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
-				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
-				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
-				minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
-			color: 'red'
-		},
-		{
-			title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Laudantium minima iusto, consequuntur expedita quaerat illo velit totam',
-			category: 'business',
-			content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
-				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
-				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
-				minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
-			color: 'red'
-		},
-		{
-			title: 'Lorem ipsum, dolor sit, amet consectetur adipisicing elitvelit totam',
-			category: 'work',
-			content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima dolor non animi culpa tempora. 
-				A debitis quod accusamus rerum dolorem pariatur vel illo non fuga necessitatibus fugit velit delectus expedita 
-				fugiat beatae, aperiam molestias ea incidunt tenetur aliquam corporis nemo minus vero id. Non consequatur iure, optio aut eos 
-				minima eaque tempore, quas laudantium suscipit repellendus, similique animi beatae`,
-			color: 'pink'
-		},
-	]
+		}]
+	},
+	newNote: {
+		content: "",
+		editing: false
+	},
+	recents: [],
+	previewedNote: {},
+	focusedCategory: '',
+	noteCategories: ['business', 'work', 'school'],
 }
