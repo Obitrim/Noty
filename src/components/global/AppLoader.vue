14dedc24f584cc212db86e1a4203c3a2590cb6fc
obitrim <pevangelist516@gmail.com>
2020-09-30 23:59:12 +0000

    added directory for global components

---
+++ src/components/global/AppLoader.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="AppLoaderDialog d-flex justify-content-center align-items-center" v-if="$store.state.loading">
+		<div class="AppLoader bg-white px-3 py-2 d-flex align-items-center">Please wait...</div>
+	</div>
+</template>
